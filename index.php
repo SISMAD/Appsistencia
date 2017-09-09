5e56ec9e294d4441958f78b9598d20fdae98a831
Nx0Hackrix <nx0Hackrix@gmail.com>
2017-09-09 17:43:57 -0500

    iniciacion d eindex
    iniciacion d eindex

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+<main class="mdl-layout__content">
+
+<body>
+    <div class="mdl-layout mdl-layout--fixed-heade
+    
+    
+    r mdl-js-layout mdl-color--grey-100">
+      <header class="mdl-layout__header mdl-layout__header--scroll mdl-color--grey-100 mdl-color-text--grey-800">
+        <div class="mdl-layout__header-row">
+          <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+            <label class="mdl-button mdl-js-button mdl-button--icon" for="search">
+              <i class="material-icons">search</i>
+            </label>
+          </div>
+        </div>
+      </header>
