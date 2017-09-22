e85c731025bb80984b72645a99c5910726ebe756
Nx0Hackrix <nx0hackrix@gmail.com>
2017-09-21 22:13:13 -0500

    Insercion de la sesionPHP

--- ValidarLogin.php
+++ ValidarLogin.php
@@ -1,6 +1,4 @@
 <?php 
-
-
 require("Conexion.php");
 
 $_codigo=mysqli_real_escape_string($conexion,trim($_POST["codigo"]));
@@ -10,7 +8,9 @@ $control=false;
 if (empty($_codigo)){
 	echo "<script languaje=\"javascript\">alert('NO HA LLENADO EL CAMPO OBLIGATORIO Digite su Código ');</script>";
 	echo "<script>document.location.href='http://localhost/Software_Registro_Loguin/Index.html';</script>\n";
-}else{
+}
+
+else{
 	$con1="SELECT codigo_udc,contraseña FROM login WHERE codigo_udc=?";
 	
 	$obj1=mysqli_prepare($conexion,$con1);
@@ -21,7 +21,9 @@ if (empty($_codigo)){
 	
 	if($ok1==false){
 		echo "ERROR EN LA EJECUCIÓN DE ACCESO <br><br>";
-	}else{
+	}
+	
+	else{
 			$ok1=mysqli_stmt_bind_result($obj1,$usu,$pass);   //VALIDACIÓN DE USUARIO
 			while (mysqli_stmt_fetch($obj1)) {
 			   
@@ -43,52 +45,5 @@ if (empty($_codigo)){
 	   }    
    }
 
-   
-
-   
-	/*if($unir==false || $unir3==false){
-		echo "<script languaje=\"javascript\">alert('ERROR EN LA EJECUCIÓN DE ACCESO');</script>";
-	}else{
-			
-		if (mysqli_stmt_fetch($objeto)) {                     //VALIDACION DE USUARIO
-		   $unir3=mysqli_stmt_bind_result($objeto3,$pass);   //VALIDACIÓN DE CONTRASEÑA
-			while (mysqli_stmt_fetch($objeto3)) {
-				echo "PRUEBA SÍ SE OBTUVO RESULTADO <br><br>";    //SE OBTIENE EL VALOR DE LA BBDD
-				echo "La Contraseña es:  $pass<br><br>";
-				$control=true;
-			}
-
-			if ($control==true) {
-				echo "LA CONTRASEÑA ES CORRECTA";
-			}else{
-				echo "LA CONTRASEÑA ES INCORRECTA";
-			}
-			
-
-		}else{
-			echo "<script languaje=\"javascript\">alert('NO EXISTE ESTE USUARIO, DEBE REGISTRARSE PARA INGRESAR');</script>";
-			echo "<script>document.location.href='http://localhost/Software_Registro_Loguin/Index.html';</script>\n";
-		}
-		
-	}*/
-	 // msqli_stmt_close($objeto);
-	/*$busqueda = "SELECT * FROM login WHERE codigo_udc='$_codigo'";
-	$ejecucion = mysqli_query($conexion,$busqueda);
-	
-	if($registro = mysqli_fetch_array($ejecucion)){
-	   if($_clave==$registro['contraseña']){
-			header("location: Ejemplo8.html");
-		} else {	
-			echo "<script languaje=\"javascript\">alert('LA CONTRASEÑA ES INCORRECTA');</script>";
-			echo "<script>document.location.href='http://localhost/Software_Registro_Loguin/Index.html';</script>\n";
-		}
-	}else{
-		echo "<script languaje=\"javascript\">alert('NO EXISTE ESTE USUARIO, DEBE REGISTRARSE PARA INGRESAR');</script>";
-		echo "<script>document.location.href='http://localhost/Software_Registro_Loguin/Index.html';</script>\n";
-	}*/
-
-	
-
-	mysqli_close($conexion);
-
-	?>
+mysqli_close($conexion);
+?>
--- index.php
+++ index.php
@@ -1,3 +1,54 @@
+<?php
+
+require("Conexion.php");
+
+$_codigo=mysqli_real_escape_string($conexion,trim($_POST["codigo"]));
+$_clave=mysqli_real_escape_string($conexion,trim($_POST["password"]));
+$control=false;
+
+if (empty($_codigo)){
+	echo "<script languaje=\"javascript\">alert('NO HA LLENADO EL CAMPO OBLIGATORIO Digite su Código ');</script>";
+	echo "<script>document.location.href='http://localhost/Software_Registro_Loguin/Index.html';</script>\n";
+}
+
+else{
+	$con1="SELECT codigo_udc,contraseña FROM login WHERE codigo_udc=?";
+	
+	$obj1=mysqli_prepare($conexion,$con1);
+	
+	$ok1=mysqli_stmt_bind_param($obj1,"s",$_codigo);
+	
+	$ok1=mysqli_stmt_execute($obj1);
+	
+	if($ok1==false){
+		echo "ERROR EN LA EJECUCIÓN DE ACCESO <br><br>";
+	}
+	
+	else{
+			$ok1=mysqli_stmt_bind_result($obj1,$usu,$pass);   //VALIDACIÓN DE USUARIO
+			while (mysqli_stmt_fetch($obj1)) {
+			   
+			   $control=true;
+			   if (password_verify($_clave,$pass)) {
+						echo "PRUEBA SÍ SE OBTUVO RESULTADO <br><br>";    //SE OBTIENE EL VALOR DE LA BBDD
+						echo "La Contraseña de la BBDD es:  $pass<br><br>";
+						
+					}else{
+					  echo "<script languaje=\"javascript\">alert('LA CONTRASEÑA ES INCORRECTA');</script>";
+					  echo "<script>document.location.href='/appsistencia';</script>\n";
+				  }
+
+			  }
+
+			  if ($control==false) {
+			   echo "<script>document.location.href='UsuarioNoExiste.html';</script>\n";
+		   }
+	   }    
+   }
+
+mysqli_close($conexion);
+?>
+
 <html lang="es" class="mdl-js">
 <head>
 	<meta charset="utf-8">
@@ -11,7 +62,7 @@
 </head>
 <body>
 	<section>
-		<form action="ValidarLogin.php" method="post">
+		<form action="<?php echo htmlentities($_SERVER ['PHP_SELF']); ?>" method="post">
 			<table align="center">
 				<tr align="right">
 					<td align="right">
