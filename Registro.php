98ddb861476a0cc9df20920abcc317090edc07ab
Luis Avila <avilaluis0@hotmail.com>
2017-09-22 02:05:59 -0500

    Corregida identacion y cambios menores en la estetica del codigo

--- Registro.php
+++ Registro.php
@@ -1,63 +1,52 @@
 <?php 
-
-if(empty($_POST["Enviar_fer"])){
-
-	
-	$bdsc_clave=trim($_POST["password"]);
-	$bd_reclave=trim($_POST["repassword"]);
-	$bd_rol='1';     
-
-	if(!($bdsc_clave==$bd_reclave)){
-	   echo "<script languaje=\"javascript\">alert('LAS CONTRASEÑAS NO COINCIDEN');</script>";
-	   echo "<script>document.location.href='registro.html';</script>\n";
-   }else{
-	require("Conexion.php");
-	
-	$bd_codigo=mysqli_real_escape_string($conexion,trim($_POST["codigo"]));
-	$bd_nombre1=mysqli_real_escape_string($conexion,trim($_POST["nombre1"]));
-	$bd_nombre2=mysqli_real_escape_string($conexion,trim($_POST["nombre2"]));
-	$bd_apellido1=mysqli_real_escape_string($conexion,trim($_POST["apellido1"]));
-	$bd_apellido2=mysqli_real_escape_string($conexion,trim($_POST["apellido2"]));
-	$bd_email=mysqli_real_escape_string($conexion,trim($_POST["correo"]));
-	$bd5_clave=mysqli_real_escape_string($conexion,$bdsc_clave);
-	$bd_clave=password_hash($bd5_clave,PASSWORD_DEFAULT,array('cost' =>12));
-
-	if (empty($bd_codigo)||empty($bd_nombre1)||empty($bd_apellido1)||empty($bd_email)||empty($bd_clave)){
-		echo "<script languaje=\"javascript\">alert('NO HA LLENADO ALGUNO DE LOS CAMPOS OBLIGATORIOS: Código de la UDC, Primer Nombre, Primer Apellido, Email y Contraseñas');</script>";
+if (empty($_POST["Enviar_fer"])) {
+	$bdsc_clave = trim($_POST["password"]);
+	$bd_reclave = trim($_POST["repassword"]);
+	$bd_rol = '1';     
+	if (!($bdsc_clave == $bd_reclave)) {
+		echo "<script languaje=\"javascript\">alert('LAS CONTRASEÑAS NO COINCIDEN');</script>";
 		echo "<script>document.location.href='registro.html';</script>\n";
 	}else{
-
-	   $consulta="SELECT * FROM personal WHERE codigo_udc='$bd_codigo'";
-	   $resultado=mysqli_query($conexion,$consulta);
-	   $fila=mysqli_fetch_row($resultado);
-
-	   if($fila>0){
-
-		  echo "<script languaje=\"javascript\">alert('YA EXISTE ESE CÓDIGO');</script>";
-		  echo "<script>document.location.href='registro.html';</script>\n";
-	  }else{
-
-	   $sql1="CALL registro_personal(?,?,?,?,?,?,?)";
-	   $sql2="CALL registro_login(?,?)";
-	   $objeto1=mysqli_prepare($conexion,$sql1);
-	   $objeto2=mysqli_prepare($conexion,$sql2);
-	   $unir1=mysqli_stmt_bind_param($objeto1,"sssssss",$bd_codigo,$bd_nombre1,$bd_nombre2,$bd_apellido1,$bd_apellido2,$bd_email,$bd_rol);
-	   $unir2=mysqli_stmt_bind_param($objeto2,"ss",$bd_codigo,$bd_clave);
-	   $unir1=mysqli_stmt_execute($objeto1);
-	   $unir2=mysqli_stmt_execute($objeto2);
-	   if($unir1==false || $unir2==false){
-		echo "<script languaje=\"javascript\">alert('ERROR AL EJECUTAR EL REGISTRO');</script>";
-	}else{
-		echo "<script languaje=\"javascript\">alert('REGISTRO EXITOSO');</script>";
-		echo "<script>document.location.href='/appsistencia';</script>\n";
-	}
+		require("Conexion.php");
+		$bd_codigo = mysqli_real_escape_string($conexion, trim($_POST["codigo"]));
+		$bd_nombre1 = mysqli_real_escape_string($conexion, trim($_POST["nombre1"]));
+		$bd_nombre2 = mysqli_real_escape_string($conexion, trim($_POST["nombre2"]));
+		$bd_apellido1 = mysqli_real_escape_string($conexion, trim($_POST["apellido1"]));
+		$bd_apellido2 = mysqli_real_escape_string($conexion, trim($_POST["apellido2"]));
+		$bd_email = mysqli_real_escape_string($conexion, trim($_POST["correo"]));
+		$bd5_clave = mysqli_real_escape_string($conexion, $bdsc_clave);
+		$bd_clave = password_hash($bd5_clave, PASSWORD_DEFAULT, array('cost' => 12));
+		if (empty($bd_codigo) || empty($bd_nombre1)|| empty($bd_apellido1)|| empty($bd_email) || empty($bd_clave)) {
+			echo "<script languaje=\"javascript\">alert('NO HA LLENADO ALGUNO DE LOS CAMPOS OBLIGATORIOS: Código de la UDC, Primer Nombre, Primer Apellido, Email y Contraseñas');</script>";
+			echo "<script>document.location.href='registro.html';</script>\n";
+		}else{
+			$consulta = "SELECT * FROM personal WHERE codigo_udc='$bd_codigo'";
+			$resultado = mysqli_query($conexion, $consulta);
+			$fila = mysqli_fetch_row($resultado);
+			if ($fila > 0) {
+				echo "<script languaje=\"javascript\">alert('YA EXISTE ESE CÓDIGO');</script>";
+				echo "<script>document.location.href='registro.html';</script>\n";
+			}else{
+				$sql1="CALL registro_personal(?,?,?,?,?,?,?)";
+				$sql2 = "CALL registro_login(?,?)";
+				$objeto1 = mysqli_prepare($conexion, $sql1);
+				$objeto2 = mysqli_prepare($conexion, $sql2);
+				$unir1 = mysqli_stmt_bind_param($objeto1, "sssssss", $bd_codigo, $bd_nombre1, $bd_nombre2, $bd_apellido1, $bd_apellido2, $bd_email, $bd_rol);
+				$unir2 = mysqli_stmt_bind_param($objeto2, "ss", $bd_codigo, $bd_clave);
+				$unir1 = mysqli_stmt_execute($objeto1);
+				$unir2 = mysqli_stmt_execute($objeto2);
+				if ($unir1 == false || $unir2 == false) {
+					echo "<script languaje=\"javascript\">alert('ERROR AL EJECUTAR EL REGISTRO');</script>";
+				}else{
+					echo "<script languaje=\"javascript\">alert('REGISTRO EXITOSO');</script>";
+					echo "<script>document.location.href='/appsistencia';</script>\n";
 						 /*mysqli_query($conexion,"CALL registro_personal('$bd_codigo','$bd_nombre1','$bd_nombre2','$bd_apellido1','$bd_apellido2','$bd_email','1')");
 						 mysqli_query($conexion,"CALL registro_login('$bd_codigo','$bd_clave')");*/
 						 /*echo "<script languaje=\"javascript\">alert('REGISTRO EXITOSO');</script>";
 						 echo "<script>document.location.href='http://localhost/Software_Registro_Loguin/Index.html';</script>\n";*/
+						}
 					}
+					mysqli_close($conexion);
 				}
-				mysqli_close($conexion);
 			}
-		}
-		?>
+			?>
--- index.php
+++ index.php
@@ -1,61 +1,42 @@
 <?php
-	
-	require("Conexion.php");
+require("Conexion.php");
 
-	if(!empty($_POST))
-	{
-		$codigo = mysqli_real_escape_string($_POST['codigo']);
-		$password = mysqli_real_escape_string($_POST['password']);
+$_codigo = mysqli_real_escape_string($conexion, trim($_POST["codigo"]));
+$_clave = mysqli_real_escape_string($conexion, trim($_POST["password"]));
+$control = false;
 
-	}
-
-$_codigo=mysqli_real_escape_string($conexion,trim($_POST["codigo"]));
-$_clave=mysqli_real_escape_string($conexion,trim($_POST["password"]));
-$control=false;
-
-if (empty($_codigo)){
+if (empty($_codigo)) {
 	echo "<script languaje=\"javascript\">alert('NO HA LLENADO EL CAMPO OBLIGATORIO Digite su Código ');</script>";
 	echo "<script>document.location.href='http://localhost/Software_Registro_Loguin/Index.html';</script>\n";
-}
-
-else{
-	$con1="SELECT codigo_udc,contraseña FROM login WHERE codigo_udc=?";
+}else{
+	$con1 = "SELECT codigo_udc,contraseña FROM login WHERE codigo_udc=?";
 	
-	$obj1=mysqli_prepare($conexion,$con1);
+	$obj1 = mysqli_prepare($conexion, $con1);
 	
-	$ok1=mysqli_stmt_bind_param($obj1,"s",$_codigo);
+	$ok1 = mysqli_stmt_bind_param($obj1,"s", $_codigo);
 	
-	$ok1=mysqli_stmt_execute($obj1);
+	$ok1 = mysqli_stmt_execute($obj1);
 	
-	if($ok1==false){
+	if ($ok1 == false) {
 		echo "ERROR EN LA EJECUCIÓN DE ACCESO <br><br>";
-	}
-	
-	else{
-			$ok1=mysqli_stmt_bind_result($obj1,$usu,$pass);   //VALIDACIÓN DE USUARIO
-			while (mysqli_stmt_fetch($obj1)) {
-			   
-			   $control=true;
-			   if (password_verify($_clave,$pass)) {
-				   	$_SESSION["estudiante"]=1;
-					   echo "<script>document.location.href='/appsistencia/Estudiante.php';</script>\n";
-					}
-					else{
-					  echo "<script languaje=\"javascript\">alert('LA CONTRASEÑA ES INCORRECTA');</script>";
-					  echo "<script>document.location.href='/appsistencia';</script>\n";
-				  }
-
-			  }
-
-			  if ($control==false) {
-			   echo "<script>document.location.href='UsuarioNoExiste.html';</script>\n";
-		   }
-	   }    
-   }
-
+	}else{
+		$ok1 = mysqli_stmt_bind_result($obj1, $usu, $pass);   //VALIDACIÓN DE USUARIO
+		while (mysqli_stmt_fetch($obj1)) {
+			$control = true;
+			if (password_verify($_clave, $pass)) {
+				echo "<script>document.location.href='/appsistencia/Estudiante.php';</script>\n";
+			}else{
+				echo "<script languaje=\"javascript\">alert('LA CONTRASEÑA ES INCORRECTA');</script>";
+				echo "<script>document.location.href='/appsistencia';</script>\n";
+			}
+		}
+		if ($control == false) {
+			echo "<script>document.location.href='UsuarioNoExiste.html';</script>\n";
+		}
+	}    
+}
 mysqli_close($conexion);
 ?>
-
 <html lang="es" class="mdl-js">
 <head>
 	<meta charset="utf-8">
@@ -81,23 +62,21 @@ mysqli_close($conexion);
 				div.parrafos {text-align: center}
 			</STYLE>
 			<table align="center">
-			  <h2 align="center">APPSISTENCIA</h2>
-			  <div class="parrafos">
-
-				<br></br>
-				<input type="number" name="codigo" pattern="[0-9]{1,15}" placeholder="Codigo" title="Ej. 4150510000" required>
-				<br></br>
-
-				<input type="password" name="password" pattern="[A-Za-z0-9._-]{1,15}" required placeholder="Contraseña" title="Digita una contraseña" required>
-				<br></br>
-				<a href="registro.html"><input type="button" value="Registrarse" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"></a>
-				<!-- <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" title="Registrarse"> -->
-			</button>
-		</a>
-		<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="submit" name="Enviar" title="Ingresar">Conectarse</button>
-		<h5><a href="#">¿Olvidaste contraseña?</a></h5>
-	</table>
-</form>                      
+				<h2 align="center">APPSISTENCIA</h2>
+				<div class="parrafos">
+					<br></br>
+					<input type="number" name="codigo" pattern="[0-9]{1,15}" placeholder="Codigo" title="Ej. 4150510000" required>
+					<br></br>
+					<input type="password" name="password" pattern="[A-Za-z0-9._-]{1,15}" required placeholder="Contraseña" title="Digita una contraseña" required>
+					<br></br>
+					<a href="registro.html"><input type="button" value="Registrarse" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"></a>
+					<!-- <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" title="Registrarse"> -->
+				</button>
+			</a>
+			<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="submit" name="Enviar" title="Ingresar">Conectarse</button>
+			<h5><a href="#">¿Olvidaste contraseña?</a></h5>
+		</table>
+	</form>                      
 </section>
 </body>
 </html>
