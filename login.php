a3e951f203d6b5532c531914f81e7ac7a13d1caf
Nx0Hackrix <nx0hackrix@gmail.com>
2017-09-28 01:01:14 -0500

    Se asignan comentarios a las funciones importantes de PHP

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if (!empty($_POST)) {
 	if (empty($_codigo)) {
 		echo "<script languaje=\"javascript\">alert('NO HA LLENADO EL CAMPO OBLIGATORIO - Digite su Código ');</script>";
 	}else{
-		$con1 = "SELECT codigo_udc,contraseña FROM login WHERE codigo_udc=?";
+		$con1 = "SELECT codigo_udc,contraseña FROM login WHERE codigo_udc=?"; // Consulta del Usuario y Contraseña
 		$obj1 = mysqli_prepare($conexion, $con1);
 		$ok1 = mysqli_stmt_bind_param($obj1,"s", $_codigo);
 		$ok1 = mysqli_stmt_execute($obj1);
@@ -25,13 +25,13 @@ if (!empty($_POST)) {
 
 			if (password_verify($_clave, $pass)) {
 				mysqli_stmt_close($obj1);
-				$con2 = "SELECT rol,nombre1,nombre2,apellido1,apellido2,email FROM personal WHERE codigo_udc=?";
+				$con2 = "SELECT rol,nombre1,nombre2,apellido1,apellido2,email FROM personal WHERE codigo_udc=?"; //Consulta para traer los datos del Usuario
 				$obj2 = mysqli_prepare($conexion, $con2);
 				$ok2 = mysqli_stmt_bind_param($obj2,"s", $_codigo);
 				$ok2 = mysqli_stmt_execute($obj2);
 				$ok2 = mysqli_stmt_bind_result($obj2, $rol, $nombre1, $nombre2, $apellido1, $apellido2, $email);
 
-				if (mysqli_stmt_fetch($obj2)) {
+				if (mysqli_stmt_fetch($obj2)) { // Se Almacenan los datos de la consulta en variables de sesion para que queden disponibles
 					$_SESSION['codigo'] = $_codigo;
 					$_SESSION['rol'] = $rol;
 					$_SESSION['nombre1'] = $nombre1;
@@ -40,7 +40,7 @@ if (!empty($_POST)) {
 					$_SESSION['apellido2'] = $apellido2;
 					$_SESSION['email'] = $email;
 
-					switch ($_SESSION['rol']) {
+					switch ($_SESSION['rol']) { // Redirecciona a su plataforma correspondiente segun su Rol
 						case '1':
 						header('Location: alumno.php');
 						break;
@@ -68,7 +68,7 @@ if (!empty($_POST)) {
 }
 mysqli_close($conexion);
 
-}elseif (!empty($_SESSION['codigo'])) {
+}elseif (!empty($_SESSION['codigo'])) {  	//Validacion si la sesion ya existe y al usuario se le da por ir 'Atras'
 	switch ($_SESSION['rol']) {
 		case '1':
 		header('Location: alumno.php');
@@ -78,6 +78,7 @@ mysqli_close($conexion);
 		break;
 		case '3':
 		header('Location: coordinador.php');
+		break;
 		case '4':
 		header('Location: admin/index.php');
 		break;
@@ -87,7 +88,7 @@ mysqli_close($conexion);
 	?>
 	<html lang="es" class="mdl-js">
 	<head>
-		<meta charset="utf-8">
+		<meta charset="utf-8"> 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="Appsistencia" content="Control de asistencia de las tutorias de los docentes">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
@@ -114,7 +115,7 @@ mysqli_close($conexion);
 						<br></br>
 						<input type="tel" name="codigo" pattern="[0-9]{1,15}" placeholder="Ej. 4150510000" data-toggle="tooltip" title="Ej. 4150510000" required autofocus>
 						<br></br>
-						<input type="password" name="password" pattern="[A-Za-z0-9._-]{1,15}" required placeholder="Contraseña" title="Digita una contraseña" required>
+						<input type="password" name="password" pattern="[A-Za-z0-9._-]{1,15}" required placeholder="Contraseña" title="Digita una contraseña" required> <!-- Validar los caracteres especiales -->
 						<br></br>
 						<a href="registro.html"><input type="button" value="Registrarse" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"></a>
 					</button>
